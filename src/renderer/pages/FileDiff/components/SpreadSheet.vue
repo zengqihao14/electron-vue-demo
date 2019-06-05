b8414c99f4a40a92a86ccfdb34fc29fd3e059a10
zengqihao14 <zengqihao0104@gmail.com>
2019-06-05 16:38:21 +0800

    fix error

--- src/renderer/pages/FileDiff/components/SpreadSheet.vue
+++ src/renderer/pages/FileDiff/components/SpreadSheet.vue
@@ -1,9 +1,14 @@
 <template lang="pug">
 	section.spreadsheet-wrapper
-		.spreadsheet-contents(v-html="htmlXLSX")
+		md-button.md-raised.save-btn(@click="saveToXLS") Export
+		.spreadsheet-contents(
+			v-html="htmlXLSX"
+			ref="htmlContents"
+		)
 </template>
 
 <script>
+  import { ipcRenderer } from 'electron'
   import XLSX from 'xlsx'
 
   export default {
@@ -30,10 +35,48 @@
             console.log(e)
           }
         }
+      },
+      saveToXLS() {
+        ipcRenderer.send('open-directory-dialog', ['openDirectory', 'createDirectory', 'promptToCreate'])
+        ipcRenderer.on('selectedPath', this.handleDirectoryFetched)
+      },
+      handleDirectoryFetched(e, path) {
+        if (path === null) {
+          console.log('请选择一个文件/文件夹')
+        } else {
+          ipcRenderer.send('open-savefile-dialog', {
+            defaultPath: path
+          })
+          ipcRenderer.on('selectedItem', this.handleSaveFile)
+        }
+      },
+      handleSaveFile(e, fullpath) {
+        if (fullpath) {
+          const htmlContents = this.$refs.htmlContents
+          const wb = XLSX.utils.table_to_book(htmlContents);
+          XLSX.writeFile(wb, `${fullpath}.xlsx`);
+        }
+      },
+      handleHTMLContentsChange() {
+        const htmlContents = this.$refs.htmlContents
+        // const xtension = 'xls|xlsx|xlsm|xlsb|xml|csv|txt|dif|sylk|slk|prn|ods|fods|htm|html'.split('|')
+        const wb = XLSX.utils.table_to_book(htmlContents);
+      },
+	    setHandleHTMLContentsChange() {
+        const htmlContents = this.$refs.htmlContents
+        htmlContents.addEventListener('DOMSubtreeModified', this.handleHTMLContentsChange);
+	    },
+      unsetHandleHTMLContentsChange() {
+        const htmlContents = this.$refs.htmlContents
+        htmlContents.removeEventListener('DOMSubtreeModified', this.handleHTMLContentsChange);
       }
     },
     mounted() {
 	    this.sheetToHtml()
+			this.setHandleHTMLContentsChange()
+    },
+	  beforeDestroy() {
+      this.unsetHandleHTMLContentsChange()
     }
   }
 </script>
@@ -48,6 +91,10 @@
 			height: 350px
 			overflow: scroll
 			margin: 10px 0
+
+			.save-btn
+				position: relative
+
 		&-contents
 			overflow: scroll
 			min-width: 1280px
